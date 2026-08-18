666cd86726ea61d17ee791f934965b66804514b3
naira-osama <nairaosama5060@gmail.com>
2026-08-18 22:29:55 +0300

    Time: 7 ms (91.65%) | Memory: 19.5 MB (46.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,6 @@
 from collections import Counter
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        s_dict = Counter(s)
-        t_dict = Counter(t)
 
-        return s_dict == t_dict
+class Solution:
+
+  def isAnagram(self, s: str, t: str) -> bool:
+    return len(s) == len(t) and Counter(s) == Counter(t)

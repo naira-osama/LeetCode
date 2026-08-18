4b488c47c2a0b226c316f998c44e63d16a517d20
naira-osama <nairaosama5060@gmail.com>
2026-08-18 22:26:26 +0300

    Time: 7 ms (91.65%) | Memory: 19.4 MB (75.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,10 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        hash1 = {}
-        for c in s:
-            if c in hash1:
-                hash1[c] +=1
-            else:
-                hash1[c] = 1
-
-        hash2 = {}
-        for c in t:
-            if c in hash2:
-                hash2[c] +=1
-            else:
-                hash2[c] = 1
-
-        return hash1 == hash2
+        if len(s) != len(t):
+            return False
         
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+
+        return s_dict == t_dict
